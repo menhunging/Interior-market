4f885899b092dca038e112d364f7d6498e46d99c
menhunging <menhunging@gmail.com>
2023-06-12 18:23:56 +0300

    add grettings block

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
-
-})
+  if ($(".grettings__slider").length > 0) {
+    let swiper = new Swiper(".grettings__slider", {
+      slidesPerView: 1,
+      effect: "creative",
+      creativeEffect: {
+        prev: {
+          shadow: true,
+          translate: ["-120%", 0, -500],
+        },
+        next: {
+          shadow: true,
+          translate: ["120%", 0, -500],
+        },
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
+});
