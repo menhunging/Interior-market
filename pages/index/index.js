2253be2ae79d843f57a4f0f53039ef1a6c396748
menhunging <menhunging@gmail.com>
2023-06-13 17:01:00 +0300

    add slider main

--- pages/index/index.js
+++ pages/index/index.js
@@ -19,4 +19,51 @@ $(document).ready(function () {
       },
     });
   }
+
+  if ($(".slider-caterogy").length > 0) {
+    const sliders = document.querySelectorAll(".slider-caterogy");
+    let mySwipers = [];
+
+    function sliderinit() {
+      sliders.forEach((slider, index) => {
+        if (!slider.swiper) {
+          mySwipers[index] = new Swiper(slider, {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            spaceBetween: 32,
+            pagination: {
+              el: ".swiper-pagination",
+              type: "fraction",
+              formatFractionCurrent: function (number) {
+                return number;
+              },
+            },
+            on: {
+              init: function (swiper) {},
+              slideChange: function (swiper) {},
+            },
+            breakpoints: {
+              320: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                spaceBetween: 16,
+              },
+              767: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+              },
+              1200: {
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+              },
+            },
+          });
+        } else {
+          return;
+        }
+      });
+    }
+
+    sliders.length && sliderinit();
+  }
 });
